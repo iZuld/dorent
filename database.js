3bfb9b0328d230eacd3563ff74dc9b540de9bfe3
unknown <dhiaulhaq121@gmail.com>
2018-10-18 10:40:00 +0700

    COMMITMESSAGE

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const bodyParser = require('body-parser');
+/*const bodyParser = require('body-parser');
 
 
 var mongoose = require ('mongoose');
@@ -42,3 +42,4 @@ if(req.bodyParser.email &&
             }
         })
     }
+*/
