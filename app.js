eb48e651c860c90140511fd02f7b6d1a7c97efed
unknown <dhiaulhaq121@gmail.com>
2018-10-18 10:03:35 +0700

    COMMITMESSAGE

--- app.js
+++ app.js
@@ -34,6 +34,53 @@ app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/', db);
 
+
+//create schema and connet to mongoDb;
+var mongoose = require ('mongoose');
+var userSchema= new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim:true,
+    },
+    rentalname:{
+        type: String,
+        required:true,
+        trim:true
+    },
+    password:{
+        type:password,
+        required:true
+    }
+});
+
+var rentaldb = mongoose.model('rental', userSchema);
+//var user = mongoose.model('user', userSchema);
+module.exports = rentaldb;
+//module.exports = user;
+
+if(req.bodyParser.email &&
+    req.bodyParser.rentalname &&
+    req.bodyParser.password){
+
+        var userRental = {
+            email:req.bodyParser.email,
+            rentalname : req.bodyParser.rentalname,
+            password : req.bodyParser.password
+        }
+
+        rentaldb.create(userRental,function(err, user){
+            if (err) {
+                return next(err)
+            } else{
+                return res.redirect('/');
+            }
+        })
+    }
+
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -51,3 +98,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
