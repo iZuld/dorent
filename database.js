24f4435736d715561c1f002bbf7d816cabb3816d
unknown <dhiaulhaq121@gmail.com>
2018-10-18 10:21:23 +0700

    COMMIIMESSAGE

---
+++ database.js
@@ -0,0 +1,44 @@
+const bodyParser = require('body-parser');
+
+
+var mongoose = require ('mongoose');
+var userSchema= new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim:true,
+    },
+    rentalname:{
+        type: String,
+        required:true,
+        trim:true
+    },
+    password:{
+        type:password,
+        required:true
+    }
+});
+
+var rentaldb = mongoose.model('rental', userSchema);
+//var user = mongoose.model('user', userSchema);
+module.exports = rentaldb;
+//module.exports = user;
+
+if(req.bodyParser.email &&
+    req.bodyParser.rentalname &&
+    req.bodyParser.password){
+
+        var userRental = {
+            email:req.bodyParser.email,
+            rentalname : req.bodyParser.rentalname,
+            password : req.bodyParser.password
+        }
+
+        rentaldb.create(userRental,function(err, user){
+            if (err) {
+                return next(err)
+            } else{
+                return res.redirect('/');
+            }
+        })
+    }
